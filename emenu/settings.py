050f903bed9640d39a94d26d7f638d4ea305815f
Artur Zacniewski <a.zacniewski@gmail.com>
2014-05-12 10:51:19 +0200

    New settings

--- emenu/settings.py
+++ emenu/settings.py
@@ -4,8 +4,9 @@
 import os
 SETTINGS_DIR = os.path.dirname(__file__)
 
-PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
-PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+#PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+#PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))  # Heroku's tip
 
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
@@ -36,7 +37,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Heroku's tip
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -74,7 +75,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'  # Heroku's tip
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -182,14 +183,15 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = 'staticfiles'
+#STATIC_URL = '/static/'
 
+"""
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+"""
