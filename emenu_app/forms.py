bcf62dc8a65770bbe83a468d94a06ef52648a0a0
Artur Zacniewski <a.zacniewski@gmail.com>
2014-05-13 21:30:17 +0200

    Error form

--- emenu_app/forms.py
+++ emenu_app/forms.py
@@ -3,7 +3,7 @@ from emenu_app.models import Error
 
 
 class ErrorForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(widget=forms.Textarea, required=True)
     email = forms.EmailField(required=False)
 
     class Meta:
