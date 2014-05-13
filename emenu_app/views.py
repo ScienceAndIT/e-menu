b23be472ba0ab72896bea3c281e88ac2d473fb7d
Artur Zacniewski <a.zacniewski@gmail.com>
2014-05-13 10:49:18 +0200

    Menus view

--- emenu_app/views.py
+++ emenu_app/views.py
@@ -2,10 +2,13 @@
 #from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from emenu_app.models import Menu, Danie
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    menu_list = Menu.objects.all()
+    context_dict = {'menus': menu_list}
+    #context_dict = {'boldmessage': "I am bold font from the context"}
     return render_to_response('emenu_app/index.html', context_dict, context)
     #return HttpResponse("<a href='http://www.scienceandit.net'>Artur</a> says hello Heroku!")
