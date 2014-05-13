4430011f627b89c5673f061c434829ed4ae44d30
Artur Zacniewski <a.zacniewski@gmail.com>
2014-05-13 13:26:01 +0200

    Menu view

--- emenu/urls.py
+++ emenu/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
+#from django.contrib.staticfiles import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -18,7 +19,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 #urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += patterns(
         'django.views.static',
